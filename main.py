57deeb231fc203154c6051bd79441c67bb8873c6
Ibragim <72756511+Ibrakam@users.noreply.github.com>
2025-12-25 20:40:55 +0500

    sdlkjsf

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from api.comment_api import comment_router
 from api.user_api import user_router
 from api.post_api import post_router
 from database import Base, engine
+from database.postservice import get_all_posts
+from database.userservice import get_all_or_exact_user_db
 
-app = FastAPI(docs_url="/")
+app = FastAPI(docs_url="/docs")
+templates = Jinja2Templates(directory="templates")
 
 Base.metadata.create_all(engine)
 
@@ -13,5 +18,22 @@ app.include_router(user_router)
 app.include_router(comment_router)
 app.include_router(post_router)
 
+
+
+@app.get("/{uid}", response_class=HTMLResponse)
+async def main(request: Request, uid: int):
+    all_posts = get_all_posts()
+    user = get_all_or_exact_user_db(uid=uid)
+    return templates.TemplateResponse(request=request, name="index.html", 
+    context={"all_posts": all_posts, "user": user})
+
+
+@app.get("/", response_class=HTMLResponse)
+async def users(request: Request):
+    users = get_all_or_exact_user_db(uid=0)
+    return templates.TemplateResponse(request=request, name="users.html", 
+    context={"users": users})
+
+# uvicorn main:app --reload
 
 
